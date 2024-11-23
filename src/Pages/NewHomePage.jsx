6d98a22d69200403ab72a10701df1b81fbfe2050
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2024-11-23 15:13:13 +0300

    saced the cat in local storage

--- src/Pages/NewHomePage.jsx
+++ src/Pages/NewHomePage.jsx
@@ -1,12 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../Context/ShopContext";
 import "../CSS/NewHomePage.css";
 const NewHomePage = () => {
   const { category } = useContext(ProductContext);
+  const [newCat, setNewCat] = useState([]);
 
+  useEffect(() => {
+    const savedCategory = localStorage.getItem("categories");
+    if (savedCategory) {
+      setNewCat(JSON.parse(savedCategory));
+    } else if (category && category.length > 0) {
+      setNewCat(category);
+      localStorage.setItem("categories", JSON.stringify(category));
+    }
+  }, []);
   return (
     <div className="newhome-container">
-      {category.map((cat) => (
+      {newCat.map((cat) => (
         <div className="list-container">
           <img className="cat-img" src={cat.image?.src} />
           {cat.name}
