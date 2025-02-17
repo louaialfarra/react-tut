6e559af969b1760cd0104d539b3570a53626541e
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2025-02-18 01:53:22 +0300

    update the scroll rsotration bye upgrade the react and all others
    npm install -g npm-check-updates
    ncu -u
    npm install

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Header from "./Components/Header/Header";
 import { BrowserRouter, Routes, Router, Route } from "react-router-dom";
 import Cart from "./Pages/Cart";
